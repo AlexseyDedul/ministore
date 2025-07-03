cd72efc8e46a8b52ab93ebb35a4955de1a99dc53
AlexseyDedul <dedyul@gmail.com>
2025-07-03 15:28:22 +0300

    add oauth2 auth, login via browser, keycloak, cross-services getting data via jwt

--- manager-app/src/main/java/by/alexdedul/managerapp/config/ClientBeans.java
+++ manager-app/src/main/java/by/alexdedul/managerapp/config/ClientBeans.java
@@ -1,19 +1,30 @@
 package by.alexdedul.managerapp.config;
 
 import by.alexdedul.managerapp.client.RestClientProductsRestClient;
+import by.alexdedul.managerapp.security.OAuthClientHttpRequestInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class ClientBeans {
     @Bean
     RestClientProductsRestClient restClientProductsRestClient(
-            @Value("${ministore.services.catalogue.uri:http://localhost:8081}") String catalogueBaseUri
-    ) {
+            @Value("${ministore.services.catalogue.uri:http://localhost:8081}") String catalogueBaseUri,
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository,
+            @Value("${ministore.services.catalogue.registration-id:keycloak}") String registrationId) {
+
         return new RestClientProductsRestClient(RestClient.builder()
                 .baseUrl(catalogueBaseUri)
+                .requestInterceptor(
+                        new OAuthClientHttpRequestInterceptor(
+                                new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository, oAuth2AuthorizedClientRepository),
+                                registrationId))
                 .build());
     }
 }
