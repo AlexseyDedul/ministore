c58d3af6842e301665d2849f58d8ac0e55112a35
AlexseyDedul <dedyul@gmail.com>
2025-07-11 10:51:55 +0300

    add reactive mongo db

--- feedback-service/src/main/java/by/alexdedul/feedbackservice/entity/ProductReview.java
+++ feedback-service/src/main/java/by/alexdedul/feedbackservice/entity/ProductReview.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.UUID;
 
@@ -11,8 +13,10 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Document("product_review")
 public class ProductReview {
 
+    @Id
     private UUID id;
     private int productId;
     private int rating;
--- feedback-service/src/main/java/by/alexdedul/feedbackservice/repository/FavouriteProductRepository.java
+++ feedback-service/src/main/java/by/alexdedul/feedbackservice/repository/FavouriteProductRepository.java
@@ -1,15 +1,15 @@
 package by.alexdedul.feedbackservice.repository;
 
 import by.alexdedul.feedbackservice.entity.FavouriteProduct;
-import reactor.core.publisher.Flux;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Mono;
 
-public interface FavouriteProductRepository {
-    Mono<FavouriteProduct> save(FavouriteProduct favouriteProduct);
+import java.util.UUID;
+
+public interface FavouriteProductRepository extends ReactiveCrudRepository<FavouriteProduct, UUID> {
 
     Mono<Void> deleteByProductId(int productId);
 
     Mono<FavouriteProduct> findByProductId(int productId);
 
-    Flux<FavouriteProduct> findAll();
 }
