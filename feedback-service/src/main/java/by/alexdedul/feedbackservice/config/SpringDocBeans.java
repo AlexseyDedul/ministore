cc93cacab199ebd4b9df0053c6e91a9f81f58cfd
AlexseyDedul <dedyul@gmail.com>
2025-07-13 11:02:26 +0300

    add documentation

---
+++ feedback-service/src/main/java/by/alexdedul/feedbackservice/config/SpringDocBeans.java
@@ -0,0 +1,28 @@
+package by.alexdedul.feedbackservice.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.OAuthScope;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("swagger")
+public class SpringDocBeans {
+    @SecurityScheme(
+            name = "keycloak",
+            type = SecuritySchemeType.OAUTH2,
+            flows = @OAuthFlows(authorizationCode = @OAuthFlow(
+                    authorizationUrl = "${keycloak.uri}/realms/ministore/protocol/openid-connect/auth",
+                    tokenUrl = "${keycloak.uri}/realms/ministore/protocol/openid-connect/token",
+                    scopes = {
+                            @OAuthScope(name = "openid"),
+                            @OAuthScope(name = "microprofile-jwt")
+                    }
+            ))
+    )
+    public static class SpringDocSecurity {
+    }
+}
