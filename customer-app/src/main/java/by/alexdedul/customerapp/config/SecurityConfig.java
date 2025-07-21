7bb85c411edd852798dd7d902d913aa3132a6f06
AlexseyDedul <dedyul@gmail.com>
2025-07-21 16:38:56 +0200

    add admin server via spring admin and connect services

--- customer-app/src/main/java/by/alexdedul/customerapp/config/SecurityConfig.java
+++ customer-app/src/main/java/by/alexdedul/customerapp/config/SecurityConfig.java
@@ -1,15 +1,32 @@
 package by.alexdedul.customerapp.config;
 
+import jakarta.annotation.Priority;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
+    @Priority(0)
+    public SecurityWebFilterChain metricsSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(configurer -> configurer
+                        .pathMatchers("/actuator/**").hasAuthority("SCOPE_metrics")
+                        .anyExchange().authenticated())
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
+                .oauth2ResourceServer(customizer -> customizer.jwt(Customizer.withDefaults()))
+                .oauth2Client(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    @Priority(1)
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         return http
                 .authorizeExchange(configurer -> configurer.anyExchange().authenticated())
