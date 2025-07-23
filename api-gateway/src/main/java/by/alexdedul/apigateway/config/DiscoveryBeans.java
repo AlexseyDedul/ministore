add03fe704ccc04c115c5131cf591185686078fd
AlexseyDedul <dedyul@gmail.com>
2025-07-23 20:32:29 +0200

    add gateway via spring cloud gateway, include eureka, tracing, and etc

---
+++ api-gateway/src/main/java/by/alexdedul/apigateway/config/DiscoveryBeans.java
@@ -0,0 +1,54 @@
+package by.alexdedul.apigateway.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.netflix.eureka.RestClientTimeoutProperties;
+import org.springframework.cloud.netflix.eureka.http.DefaultEurekaClientHttpRequestFactorySupplier;
+import org.springframework.cloud.netflix.eureka.http.EurekaClientHttpRequestFactorySupplier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+
+import java.util.List;
+import java.util.Set;
+
+@Configuration
+@ConditionalOnProperty(name = "eureka.client.enabled", havingValue = "true", matchIfMissing = true)
+public class DiscoveryBeans {
+
+    @Bean
+    public DefaultEurekaClientHttpRequestFactorySupplier defaultEurekaClientHttpRequestFactorySupplier(
+            RestClientTimeoutProperties restClientTimeoutProperties,
+            EurekaClientHttpRequestFactorySupplier.RequestConfigCustomizer requestConfigCustomizer,
+            ReactiveClientRegistrationRepository clientRegistrationRepository,
+            ReactiveOAuth2AuthorizedClientService authorizedClientRepository
+    ){
+        AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager authorizedClientManager =
+                new AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(
+                        clientRegistrationRepository,  authorizedClientRepository);
+
+        return new DefaultEurekaClientHttpRequestFactorySupplier(
+                restClientTimeoutProperties,
+                Set.of(requestConfigCustomizer),
+                List.of((request, entity, context) -> {
+                    if(!request.containsHeader(HttpHeaders.AUTHORIZATION)){
+                        OAuth2AuthorizedClient authorizedClient = authorizedClientManager.authorize(OAuth2AuthorizeRequest
+                                    .withClientRegistrationId("discovery")
+                                    .principal("api-gateway")
+                                    .build())
+                                .block();
+                        request.setHeader(HttpHeaders.AUTHORIZATION,
+                                "Bearer %s".formatted(authorizedClient.getAccessToken().getTokenValue()));
+                    }
+                }));
+    }
+
+    @Bean
+    public EurekaClientHttpRequestFactorySupplier.RequestConfigCustomizer requestConfigCustomizer() {
+        return builder -> builder.setProtocolUpgradeEnabled(false);
+    }
+}
